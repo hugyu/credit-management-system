bb37f4d488a7cfc5fdf6bad482ca9d57ad1a496b
hgy <1500586026@qq.com>
2023-11-04 19:45:56 +0800

    注册页面的设计与完善

--- src/Login/index.tsx
+++ src/Login/index.tsx
@@ -1,7 +1,51 @@
 import "./index.scss";
-import { LockOutlined, UserOutlined } from "@ant-design/icons";
-import { Button, Checkbox, Form, Input } from "antd";
+import zxcvbn from 'zxcvbn';
+
+import {
+  LockOutlined,
+  UserOutlined,
+  InfoCircleOutlined,
+  EyeTwoTone,
+  EyeInvisibleOutlined,
+} from "@ant-design/icons";
+
+import {
+  Button,
+  Col,
+  Drawer,
+  Form,
+  Input,
+  Row,
+  Space,
+  Checkbox,
+  Tooltip,
+  Progress,
+} from "antd";
+import { useState } from "react";
+
 function Login() {
+   // 获取上下文 form 实例
+   const [form] = Form.useForm();
+   // 监听密码的改变
+  const password = Form.useWatch('pwd', form);
+  console.log(password)
+  
+  const watchStrength = (password: string): number => {
+    const analysisValue = zxcvbn(password)
+    // score得分只有0~4，且只有整数范围并没有小数
+    return (analysisValue.score + 1) * 20
+  }
+
+
+  const [open, setOpen] = useState(false);
+
+  const showDrawer = () => {
+    setOpen(true);
+  };
+
+  const onClose = () => {
+    setOpen(false);
+  };
   //   type FieldType = {
   //     username?: string;
   //     password?: string;
@@ -66,11 +110,129 @@ function Login() {
               >
                 Log in
               </Button>
-              Or <a href="">register now!</a>
+              Or &nbsp;
+              <Button onClick={showDrawer} ghost type="primary">
+                register now!
+              </Button>
             </Form.Item>
           </Form>
         </div>
       </div>
+      <Drawer
+        title="Create a new account"
+        width={450}
+        onClose={onClose}
+        open={open}
+        styles={{
+          body: {
+            paddingBottom: 80,
+          },
+        }}
+        extra={
+          <Space>
+            <Button onClick={onClose}>Cancel</Button>
+            <Button onClick={onClose} type="primary">
+              Submit
+            </Button>
+          </Space>
+        }
+      >
+        <Form layout="vertical" hideRequiredMark form={form}>
+          <Row gutter={16} justify={"center"}>
+            <Col span={12}>
+              <Form.Item
+                name="name"
+                label="Name"
+                rules={[{ required: true, message: "Please enter user name" }]}
+              >
+                <Input
+                  placeholder="Enter your username"
+                  prefix={<UserOutlined className="site-form-item-icon" />}
+                  suffix={
+                    <Tooltip title="less than 10 words">
+                      <InfoCircleOutlined
+                        style={{ color: "rgba(0,0,0,.45)" }}
+                      />
+                    </Tooltip>
+                  }
+                />
+              </Form.Item>
+            </Col>
+          </Row>
+          <Row gutter={16} justify={"center"}>
+            <Col span={12}>
+              <Form.Item
+                name="pwd"
+                label="Password"
+                rules={[
+                  { required: true, message: "Please enter user password" },
+                ]}
+              >
+                <Input.Password
+                  placeholder="input password"
+                  
+                  iconRender={(visible) =>
+                    visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />
+                  }
+                />
+              </Form.Item>
+            </Col>
+          </Row>
+          <Row gutter={16} justify={"center"}>
+            <Col span={12}>
+              <Form.Item
+                name="confirmPwd"
+                label="ConfirmPassword"
+                rules={[
+                  {
+                    required: true,
+                    message: 'Please confirm your password!',
+                  },
+                  ({ getFieldValue }) => ({
+                    validator(_, value) {
+                      if (!value || getFieldValue('pwd') === value) {
+                        return Promise.resolve();
+                      }
+                      return Promise.reject(new Error('The new password that you entered do not match!'));
+                    },
+                  }),
+                ]}
+              >
+                <Input.Password
+                  placeholder="confirm password"
+                  
+                  iconRender={(visible) =>
+                    visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />
+                  }
+                  
+                />
+              </Form.Item>
+            </Col>
+          </Row>
+          <Row gutter={16} justify={"center"}>
+            <Col span={12}>
+             
+              
+              <Progress
+                  percent={password ? watchStrength(password) : 0}
+                  steps={5}
+                  size={[35,8]}
+                  showInfo={false}
+                  strokeColor={[
+                    "#e74242",
+                    "#EFBD47",
+                    "#ffa500",
+                    "#1bbf1b",
+                    "#008000",
+                  ]}
+                />
+                
+                
+              
+            </Col>
+          </Row>
+        </Form>
+      </Drawer>
     </div>
   );
 }
